eb79bd040c29fd0e3783559b562527c7fe4c2d8a
smilejakdu <ash982416@gmail.com>
2024-06-26 22:31:36 +0900

    first commit

---
+++ src/main/kotlin/com/dra/deliveryrequestapi/DeliveryRequestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.dra.deliveryrequestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliveryRequestApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeliveryRequestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/dra/deliveryrequestapi/DeliveryRequestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dra.deliveryrequestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeliveryRequestApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
